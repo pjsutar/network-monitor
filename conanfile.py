0181269bf40ce4b6230bc777365e5b98cd773474
Pawan <pawansutar@outlook.com>
2023-06-10 17:30:31 -0700

    Add function to download file from HTTPS URL

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ class ConanPackage(ConanFile):
 
     requires = [
         ('boost/1.74.0'),
+        ("libcurl/7.87.0"),
         ('openssl/1.1.1h'),
     ]
 
---
+++ inc/network-monitor/file-downloader.h
@@ -0,0 +1,18 @@
+#include <filesystem>
+#include <string>
+
+namespace NetworkMonitor {
+	/*!	\brief	Download a file from a remote HTTPS URL
+	* 
+	*	\param	destination	The full path and filename of the output
+	*						file. The path to file must exist.
+	*	\param caCertFile	The path to the cacert.pem file to perform certificate
+	*						verification in an HTTPS connection.
+	*/
+
+	bool DownloadFile(
+		const std::string& fileUrl,
+		const std::filesystem::path& destination,
+		const std::filesystem::path& caCertFile = {}
+	);
+} // namespace NetworkMonitor
