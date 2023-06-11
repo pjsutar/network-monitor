c855f318fff21438182db1f932b961c7c2662ad9
Pawan <pawansutar@outlook.com>
2023-06-10 18:13:41 -0700

    Use JSON library to parse file

--- src/file-downloader.cpp
+++ src/file-downloader.cpp
@@ -1,45 +1,66 @@
 #include <network-monitor/file-downloader.h>
 
+#include <nlohmann/json.hpp>
+
 #include <curl/curl.h>
 
 #include <filesystem>
+#include <fstream>
 #include <stdio.h>
 #include <string>
 
 bool NetworkMonitor::DownloadFile(
-	const std::string& fileUrl,
-	const std::filesystem::path& destination,
-	const std::filesystem::path& caCertFile
+    const std::string& fileUrl,
+    const std::filesystem::path& destination,
+    const std::filesystem::path& caCertFile
 )
 {
-	// Initialize curl
-	CURL* curl{ curl_easy_init() };
-	if (curl == nullptr) {
-		return false;
-	}
+    // Initalize curl.
+    CURL* curl{ curl_easy_init() };
+    if (curl == nullptr) {
+        return false;
+    }
 
-	// Open the file
-	std::FILE* fp{ fopen(destination.string().c_str(), "wb") };
-	if (fp == nullptr) {
-		// Remember to clean up in all program parts
-		curl_easy_cleanup(curl);
-		return false;
-	}
+    // Open the file.
+    std::FILE* fp{ fopen(destination.string().c_str(), "wb") };
+    if (fp == nullptr) {
+        // Remember to clean up in all program paths.
+        curl_easy_cleanup(curl);
+        return false;
+    }
 
-	// Configure curl
-	curl_easy_setopt(curl, CURLOPT_URL, fileUrl.c_str());
-	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-	curl_easy_setopt(curl, CURLOPT_CAINFO, caCertFile.string().c_str());
-	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
-	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 2L);
-	curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+    // Configure curl.
+    curl_easy_setopt(curl, CURLOPT_URL, fileUrl.c_str());
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(curl, CURLOPT_CAINFO, caCertFile.string().c_str());
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 2L);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
 
-	// Perform the request
-	CURLcode res = curl_easy_perform(curl);
-	curl_easy_cleanup(curl);
+    // Perform the request.
+    CURLcode res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
 
-	// Close the file
-	fclose(fp);
+    // Close the file.
+    fclose(fp);
 
-	return res == CURLE_OK;
+    return res == CURLE_OK;
+}
+
+nlohmann::json NetworkMonitor::ParseJsonFile(
+    const std::filesystem::path& source
+)
+{
+    nlohmann::json parsed{};
+    if (!std::filesystem::exists(source)) {
+        return parsed;
+    }
+    try {
+        std::ifstream file{ source };
+        file >> parsed;
+    }
+    catch (...) {
+        // Will return an empty object.
+    }
+    return parsed;
 }
